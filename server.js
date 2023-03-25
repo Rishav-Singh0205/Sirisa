3d513e58211b000d3feef30021c6c100a0233c00
Rishav Singh <89840621+Rishav-Singh0205@users.noreply.github.com>
2023-03-25 19:47:16 +0530

    updated

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ import categoryRoutes from './routes/catrgoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from "cors";
 import path from 'path'
+import { fileURLToPath } from 'url'
 
 //configur env
 dotenv.config();
 
 //data base config
 connectDB();
+
+//esmodeule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 
